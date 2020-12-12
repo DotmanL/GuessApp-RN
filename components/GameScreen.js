07f3226dd022fae8ec77402702161a9c0105114e
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-12-12 21:53:16 +0100

    rendered past guesses using scrollview and also flatlist

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -1,8 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import NumberContainer from './NumberContainer';
-
+import BodyText from './BodyText';
 import Card from './Card';
 import DefaultStyles from '../constants/default-styles';
 import CustomButton from './CustomButton';
@@ -19,17 +26,34 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 };
 
+// const renderListItem = (value, numOfRound) => (
+//   <View key={value} style={styles.listItem}>
+//     <BodyText>#{numOfRound}</BodyText>
+//     <BodyText>{value}</BodyText>
+//   </View>
+// );
+
+//When using a flatlist
+const renderListItem = (listLength, itemData) => (
+  <View style={styles.listItem}>
+    <BodyText>#{listLength - itemData.index}</BodyText>
+    <BodyText>{itemData.item}</BodyText>
+  </View>
+);
+
 const GameScreen = ({ userChoice, onGameOver }) => {
-  const [currentGuess, setCurrentGuess] = useState(
-    generateRandomBetween(1, 100, userChoice)
-  );
-  const [rounds, setRounds] = useState(0);
+  const initialGuess = generateRandomBetween(1, 100, userChoice);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  // const [pastGuesses, setPastGuesses] = useState([initialGuess]);
+  //when using flatlist, expect our key to be  a string
+  const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
   useEffect(() => {
     if (currentGuess === userChoice) {
-      onGameOver(rounds);
+      onGameOver(pastGuesses.length);
     }
   }, [currentGuess, onGameOver, userChoice]);
 
@@ -46,7 +70,7 @@ const GameScreen = ({ userChoice, onGameOver }) => {
     if (direction === 'lower') {
       currentHigh.current = currentGuess;
     } else {
-      currentLow.current = currentGuess;
+      currentLow.current = currentGuess + 1;
     }
     const nextNumber = generateRandomBetween(
       currentLow.current,
@@ -54,7 +78,12 @@ const GameScreen = ({ userChoice, onGameOver }) => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
-    setRounds((curRounds) => curRounds + 1);
+    //when using flatlist, expect our key to be  a string
+    // setPastGuesses((currPastGuesses) => [nextNumber, ...currPastGuesses]);
+    setPastGuesses((currPastGuesses) => [
+      nextNumber.toString(),
+      ...currPastGuesses,
+    ]);
   };
 
   return (
@@ -69,6 +98,21 @@ const GameScreen = ({ userChoice, onGameOver }) => {
           <Ionicons name="md-add" size={24} color="white" />
         </CustomButton>
       </Card>
+      <View style={styles.listContainer}>
+        {/* <ScrollView contentContainerStyle={styles.list}>
+          {pastGuesses.map((guess, index) =>
+            renderListItem(guess, pastGuesses.length - index)
+          )}
+        </ScrollView> */}
+        <FlatList
+          keyExtractor={(item) => item}
+          data={pastGuesses}
+          renderItem={(itemData) =>
+            renderListItem(pastGuesses.length, itemData)
+          }
+          contentContainerStyle={styles.list}
+        />
+      </View>
     </View>
   );
 };
@@ -85,6 +129,26 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: '70%',
+  },
+  listContainer: {
+    width: '60%',
+    flex: 1,
+    marginVertical: 10,
+  },
+  list: {
+    flexGrow: 1,
+    // alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  listItem: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
   },
 });
 
