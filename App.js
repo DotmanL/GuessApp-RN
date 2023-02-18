2196994f90486b6a55d583df47f026c629e7bcd0
Lawal Oladotun <oladotunlawal7@gmail.com>
2023-02-18 18:38:08 +0000

    updated configs

--- App.js
+++ App.js
@@ -1,33 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
+//change to use expo-splash screen
+import AppLoading from 'expo-app-loading';
 import Header from './components/Header';
 import StartGameScreen from './components/StartGameScreen';
 import GameScreen from './components/GameScreen';
 import GameOverScreen from './components/GameOverScreen';
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-};
-
 const App = () => {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
-  const [dataLoaded, setDataLoaded] = useState(false);
 
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-        onError={(err) => console.log(err)}
-      />
-    );
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   const createNewGameHandler = () => {
